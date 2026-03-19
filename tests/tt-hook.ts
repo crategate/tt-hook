baaa5ddfb57d7d531b48ad366fce5852e63a0d40
shifkey <sevensidedsilver@gmail.com>
2026-03-19 09:14:56 -0700

    testt token working with metadata and transferhookaccount updating correctly with phantom functionality

--- tests/tt-hook.ts
+++ tests/tt-hook.ts
@@ -147,7 +147,7 @@ describe("transfer-hook", () => {
 	// Fund the sender token account with 100 tokens
 	it("Create Token Accounts and Mint Tokens", async () => {
 		// 100 tokens
-		const amount = 888 * 10 ** decimals;
+		const amount = 222 * 10 ** decimals;
 
 		const transaction = new Transaction().add(
 			createAssociatedTokenAccountInstruction(
